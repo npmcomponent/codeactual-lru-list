933448197e5d06d0b42de5d21bdd7ba82fc2db0d
David Smith <codeactual@gmail.com>
2013-02-24 18:35:30 -0800

    ADD core js-lru, initial put() tests

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /**
  * Storage agnostic LRU list.
+ *   Uses doubly-linked list.
+ *   Supports async get, set, etc.
  *
  * Based on https://github.com/rsms/js-lru
  *   Licensed under MIT.
  *   Copyright (c) 2010 Rasmus Andersson <http://hunch.se/>
  */
-module.exports = LruList;
+module.exports = {
+  LRUList: LRUList,
+  LRUEntry: LRUEntry
+};
 
 var emptyFn = function() {};
 
@@ -13,16 +18,140 @@ var emptyFn = function() {};
  * @param {object} config
  *   {number} [limit=100]
  *   {function} set
+ *     (key, value, done)
  *   {function} get
+ *     (key, done)
  *   {function} del
+ *     (key, done)
  */
-function LruList(config) {
+function LRUList(config) {
   config = config || {};
   this.size = 0;
-  this.config = {
-    limit: config.limit || 100,
+  this.tail = undefined;
+  this.head = undefined;
+  this.limit = config.limit || 100;
+  this.store = {
     set: config.set || emptyFn,
     get: config.get || emptyFn,
     del: config.del || emptyFn
   };
+  this.keymap = {};
+}
+
+function LRUEntry(key) {
+  this.key = key;
+  this.older = undefined;
+  this.newer = undefined;
+};
+
+/**
+ * @param {string} key
+ * @param {mixed} value
+ * @param {function} done
+ *   {object} Error instance or null.
+ */
+LRUList.prototype.put = function(key, value, done) {
+  var self = this;
+  function storeIODone(err) {
+    if (err) { done(err); return; }
+
+    var entry = new LRUEntry(key);
+    self.keymap = entry;
+    if (self.tail) {
+      self.tail.newer = entry;
+      entry.older = self.tail;
+    } else {
+      self.head = entry;
+    }
+    self.tail = entry;
+    if (self.size === self.limit) {
+      self.shift();
+    } else {
+      self.size++;
+      done(null);
+    }
+  }
+  this.store.set(key, value, storeIODone);
+};
+
+/**
+ * @param {function} done
+ *   {object} Error instance or null.
+ *   {mixed} Shifted LruEntry or undefined.
+ */
+LRUList.prototype.shift = function(done) {
+  var entry = this.head;
+  if (!entry) {
+    done(null);
+    return;
+  }
+
+  if (this.head.newer) {
+    this.head = this.head.newer;
+    this.head.older = undefined;
+  } else {
+    this.head = undefined;
+  }
+  entry.newer = entry.older = undefined;
+
+  var self = this;
+  function storeIODone(err) {
+    if (err) { done(err); return; }
+
+    delete self.keymap[entry.key]
+    done(null, entry)
+  }
+  this.store.del(entry.key, storeIODone);
+};
+
+/**
+ * @param {string} key
+ * @param {function} done
+ *   {object} Error instance or null.
+ *   {mixed} Value or undefined.
+ */
+LRUList.prototype.get = function(key, done) {
+  function storeIODone(err, value) {
+    if (err) { done(err); return; }
+
+    var entry = this.keymap[key];
+    if (entry === undefined) {
+      done(null);
+      return;
+    }
+    if (entry === this.tail) {
+      done(null, value);
+      return;
+    }
+
+    if (entry.newer) {
+      if (entry === this.head) {
+        this.head = entry.newer;
+      }
+      entry.newer.older = entry.older;
+    }
+    if (entry.older) {
+      entry.older.newer = entry.newer;
+    }
+
+    entry.newer = undefined;
+    entry.older = this.tail;
+
+    if (this.tail) {
+      this.tail.newer = entry;
+    }
+    this.tail = entry;
+
+    done(null, value);
+  }
+  this.store.get(key, storeIODone);
+};
+
+LRUList.prototype.toArray = function() {
+  var s = [], entry = this.head;
+  while (entry) {
+    s.push(entry.key);
+    entry = entry.newer;
+  }
+  return s;
 }
--- test.js
+++ test.js
@@ -1,16 +1,130 @@
 var chai = require('chai');
-chai.should();
+var should = chai.should();
 chai.Assertion.includeStack = true;
 
-var LruList = require('./index');
+var lruList = require('./index');
+var LRUList = lruList.LRUList;
+var LRUEntry = lruList.LRUEntry;
 
-describe('LruList', function() {
-  describe('#LruList()', function() {
-    it('should init state', function(done) {
-      var list = new LruList();
-      list.size = 0;
-      list.config.limit.should.equal(100);
+function emptyFn() {}
+
+var storeErr = new Error('store err');
+
+function getFirstCb(arr) {
+  return arr.reduce(function(memo, arg) {
+    if (memo) { return memo; }
+    if ('function' === typeof arg) { return arg; }
+  }, null);
+}
+function storeOkCb() {
+  getFirstCb([].slice.call(arguments))();
+};
+function storeErrCb() {
+  getFirstCb([].slice.call(arguments))(storeErr);
+};
+
+describe('helpers', function() {
+  describe('#getFirstCb()', function() {
+    it('should return 1st function in array', function(done) {
+      var fn1 = function() {};
+      var fn2 = function() {};
+      var arr = [1, 'foo', fn1, 2, fn2];
+      getFirstCb(arr).should.deep.equal(fn1);
       done();
+    });
+  });
+});
+
+describe('LRUList', function() {
+  before(function(done) {
+    this.key = 'foo';
+    this.value = 'bar';
+    done();
+  });
+
+  describe('#LRUList()', function() {
+    it('should init state', function(done) {
+      var list = new LRUList();
+      list.size.should.equal(0);
+      list.limit.should.equal(100);
+      list.store.set.should.be.instanceOf(Function);
+      list.store.get.should.be.instanceOf(Function);
+      list.store.del.should.be.instanceOf(Function);
+      done();
+    });
+
+    it('should accept state', function(done) {
+      var state = {
+        limit: 5,
+        set: emptyFn,
+        get: emptyFn,
+        del: emptyFn
+      };
+      var list = new LRUList(state);
+      list.size.should.equal(0);
+      list.limit.should.equal(state.limit);
+      list.store.set.should.deep.equal(state.set);
+      list.store.get.should.deep.equal(state.get);
+      list.store.del.should.deep.equal(state.del);
+      done();
+    });
+  });
+
+  describe('#LRUEntry()', function() {
+    it('should init state', function(done) {
+      var key = 'foo';
+      var entry = new LRUEntry(key);
+      entry.key.should.equal(key);
+      entry.should.have.ownProperty('older');
+      should.not.exist(entry.older);
+      entry.should.have.ownProperty('newer');
+      should.not.exist(entry.newer);
+      done();
+    });
+  });
+
+  describe('#put()', function() {
+    it('should propagate IO success', function(done) {
+      var list = new LRUList({set: storeOkCb});
+      function cb(err) {
+        should.equal(err, null);
+        done();
+      }
+      list.put(this.key, this.value, cb);
+    });
+
+    it('should propagate IO error', function(done) {
+      var list = new LRUList({set: storeErrCb});
+      function cb(err) {
+        should.equal(err, storeErr);
+        done();
+      }
+      list.put(this.key, this.value, cb);
+    });
+
+    it('should update list', function(done) {
+      var list = new LRUList({set: storeOkCb});
+      var snapshots = [];
+      function addSnapshot() {
+        snapshots.push(list.toArray());
+      }
+      function endSnapshots() {
+        addSnapshot();
+        snapshots.should.deep.equal(
+          snapshots,
+          [
+            ['a'],
+            ['b', 'a'],
+            ['c', 'b', 'a'],
+            ['d', 'c', 'b', 'a']
+          ]
+        );
+        done();
+      }
+      list.put('a', this.value, addSnapshot);
+      list.put('b', this.value, addSnapshot);
+      list.put('c', this.value, addSnapshot);
+      list.put('d', this.value, endSnapshots);
     });
   });
 });
