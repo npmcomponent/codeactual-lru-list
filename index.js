4433e0248e83f80cfb4250de5765b42880146ecf
David Smith <codeactual@gmail.com>
2013-02-27 19:08:30 -0800

    Refactor get/put list updating into new methods

--- index.js
+++ index.js
@@ -62,6 +62,48 @@ function LRUEntry(key) {
   this.newer = undefined;
 }
 
+LRUList.prototype._updateStructForPut = function(key, val, done) {
+  var entry = new LRUEntry(key); // Create new tail.
+  this.keymap[key] = entry;
+
+  if (this.tail) { // Link old tail to new tail.
+    this.tail.newer = entry;
+    entry.older = this.tail;
+  } else { // First entry.
+    this.head = entry;
+  }
+
+  this.tail = entry; // Assign new tail.
+
+  if (this.size === this.limit) { // List size exceeded. Trim head.
+    this.shift(done);
+  } else {
+    this.size++;
+    done(null);
+  }
+};
+
+LRUList.prototype.putMulti = function(pairs, done) {
+  done = done || function putDoneNoOp() {};
+  var self = this;
+
+  this.store.setMulti(pairs, function storeIODone(err) {
+    if (err) { done(err); return; } // I/O failed, maintain current list/map.
+
+    var batch = new Batch;
+
+    Object.keys(pairs).forEach(function batchKey(key) {
+      batch.push(function batchPush(taskDone) {
+        self._updateStructForPut(key, pairs[key], taskDone);
+      });
+    });
+
+    batch.end(function batchEnd(err) {
+      done(err);
+    });
+  });
+};
+
 /**
  * Append key to the list's tail. Trigger storage of the value.
  *
@@ -69,7 +111,7 @@ function LRUEntry(key) {
  *   May produce "orphaned" entries to which the key map no longer points. Then they
  *   can no longer be read/removed, and can only be pushed out by lack of use.
  *
- * @param {string|array} key
+ * @param {string} key
  * @param {mixed} val
  * @param {function} done
  *   {object} Error instance or null.
@@ -77,42 +119,9 @@ function LRUEntry(key) {
 LRUList.prototype.put = function(key, val, done) {
   done = done || function putDoneNoOp() {};
   var self = this;
-
-  function updateStruct(key, updDone) {
-    var entry = new LRUEntry(key); // Create new tail.
-    self.keymap[key] = entry;
-
-    if (self.tail) { // Link old tail to new tail.
-      self.tail.newer = entry;
-      entry.older = self.tail;
-    } else { // First entry.
-      self.head = entry;
-    }
-
-    self.tail = entry; // Assign new tail.
-
-    if (self.size === self.limit) { // List size exceeded. Trim head.
-      self.shift(updDone);
-    } else {
-      self.size++;
-      updDone(null);
-    }
-  }
-
   this.store.set(key, val, function storeIODone(err) {
     if (err) { done(err); return; } // I/O failed, maintain current list/map.
-
-    var batch = new Batch;
-
-    [].concat(key).forEach(function(key) {
-      batch.push(function(keyDone) {
-        updateStruct(key, keyDone);
-      });
-    });
-
-    batch.end(function(err) {
-      done(err);
-    });
+    self._updateStructForPut(key, val, done);
   });
 };
 
@@ -152,10 +161,52 @@ LRUList.prototype.shift = function(done) {
   });
 };
 
+
+LRUList.prototype._updateStructForGet = function(entry) {
+  if (entry.newer) { // Key has more-recently-used than it.
+    if (entry === this.head) {
+      this.head = entry.newer; // 2nd-to-head is now head.
+    }
+    // Connect adjacent entries to fill the future gap it will leave.
+    entry.newer.older = entry.older;
+  }
+  if (entry.older) { // Key has less-recently-used than it.
+    // Connect adjacent entries to fill the future gap it will leave.
+    entry.older.newer = entry.newer;
+  }
+
+  entry.newer = undefined; // Entry will be newest.
+
+  // Move current tail will 2nd-to-tail positiion.
+  entry.older = this.tail;
+  if (this.tail) { this.tail.newer = entry; }
+
+  this.tail = entry;
+};
+
+LRUList.prototype.getMulti = function(keys, done) {
+
+    // try to pull all keys/entries from the keymap
+    // only hit storage.get for the misses
+
+    var keys = [].concat(isMultiKey ? Object.keys(val) : key);
+    var batch = new Batch;
+
+    keys.forEach(function batchKey(key) {
+      batch.push(function batchPush(taskDone) {
+        updateStruct(key, taskDone);
+      })
+    });
+
+    batch.end(function batchEnd(err) {
+      done(err, val);
+    });
+};
+
 /**
  * Promote the key to the tail (MRU). Read the value from storage.
  *
- * @param {string} key
+ * @param {string|array} key
  * @param {function} done
  *   {object} Error instance or null.
  *   {mixed} Value or undefined.
@@ -168,30 +219,10 @@ LRUList.prototype.get = function(key, done) {
     if (err) { done(err); return; } // I/O failed, maintain current list/map.
 
     var entry = self.keymap[key];
-
     if (entry === undefined) { done(null); return; } // Key miss.
     if (entry === self.tail) { done(null, val); return; } // Key already MRU.
 
-    if (entry.newer) { // Key has more-recently-used than it.
-      if (entry === self.head) {
-        self.head = entry.newer; // 2nd-to-head is now head.
-      }
-      // Connect adjacent entries to fill the future gap it will leave.
-      entry.newer.older = entry.older;
-    }
-    if (entry.older) { // Key has less-recently-used than it.
-      // Connect adjacent entries to fill the future gap it will leave.
-      entry.older.newer = entry.newer;
-    }
-
-    entry.newer = undefined; // Entry will be newest.
-
-    // Move current tail will 2nd-to-tail positiion.
-    entry.older = self.tail;
-    if (self.tail) { self.tail.newer = entry; }
-
-    self.tail = entry;
-
+    self._updateStructForGet(entry);
     done(null, val);
   });
 };
