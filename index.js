4c57c4cade15c78da53e32151569ae3d04216c01
David Smith <codeactual@gmail.com>
2013-02-27 22:36:48 -0800

    Reorg methods

--- index.js
+++ index.js
@@ -66,32 +66,24 @@ function LRUEntry(key) {
 }
 
 /**
- * Apply a put/putMulti operation to the list/map structures.
+ * Append key to the list's tail. Trigger storage of the value.
+ *
+ * - Duplicate keys are allowed by original design.
+ *   May produce "orphaned" entries to which the key map no longer points. Then they
+ *   can no longer be read/removed, and can only be pushed out by lack of use.
  *
  * @param {string} key
  * @param {mixed} val
  * @param {function} done
  *   {object} Error instance or null.
  */
-LRUList.prototype._updateStructForPut = function(key, val, done) {
-  var entry = new LRUEntry(key); // Create new tail.
-  this.keymap[key] = entry;
-
-  if (this.tail) { // Link old tail to new tail.
-    this.tail.newer = entry;
-    entry.older = this.tail;
-  } else { // First entry.
-    this.head = entry;
-  }
-
-  this.tail = entry; // Assign new tail.
-
-  if (this.size === this.limit) { // List size exceeded. Trim head.
-    this.shift(done);
-  } else {
-    this.size++;
-    done(null);
-  }
+LRUList.prototype.put = function(key, val, done) {
+  done = done || function putDoneNoOp() {};
+  var self = this;
+  this.store.set(key, val, function storeIODone(err) {
+    if (err) { done(err); return; } // I/O failed, maintain current list/map.
+    self._updateStructForPut(key, val, done);
+  });
 };
 
 /**
@@ -127,24 +119,32 @@ LRUList.prototype.putMulti = function(pairs, done) {
 };
 
 /**
- * Append key to the list's tail. Trigger storage of the value.
- *
- * - Duplicate keys are allowed by original design.
- *   May produce "orphaned" entries to which the key map no longer points. Then they
- *   can no longer be read/removed, and can only be pushed out by lack of use.
+ * Apply a put/putMulti operation to the list/map structures.
  *
  * @param {string} key
  * @param {mixed} val
  * @param {function} done
  *   {object} Error instance or null.
  */
-LRUList.prototype.put = function(key, val, done) {
-  done = done || function putDoneNoOp() {};
-  var self = this;
-  this.store.set(key, val, function storeIODone(err) {
-    if (err) { done(err); return; } // I/O failed, maintain current list/map.
-    self._updateStructForPut(key, val, done);
-  });
+LRUList.prototype._updateStructForPut = function(key, val, done) {
+  var entry = new LRUEntry(key); // Create new tail.
+  this.keymap[key] = entry;
+
+  if (this.tail) { // Link old tail to new tail.
+    this.tail.newer = entry;
+    entry.older = this.tail;
+  } else { // First entry.
+    this.head = entry;
+  }
+
+  this.tail = entry; // Assign new tail.
+
+  if (this.size === this.limit) { // List size exceeded. Trim head.
+    this.shift(done);
+  } else {
+    this.size++;
+    done(null);
+  }
 };
 
 /**
@@ -184,30 +184,27 @@ LRUList.prototype.shift = function(done) {
 };
 
 /**
- * Apply a get/getMulti operation to the list/map structures.
+ * Promote the key to the tail (MRU). Read the value from storage.
  *
- * @param {object} entry LRUEntry instance.
+ * @param {string|array} key
+ * @param {function} done
+ *   {object} Error instance or null.
+ *   {mixed} Value or undefined.
  */
-LRUList.prototype._updateStructForGet = function(entry) {
-  if (entry.newer) { // Key has more-recently-used than it.
-    if (entry === this.head) {
-      this.head = entry.newer; // 2nd-to-head is now head.
-    }
-    // Connect adjacent entries to fill the future gap it will leave.
-    entry.newer.older = entry.older;
-  }
-  if (entry.older) { // Key has less-recently-used than it.
-    // Connect adjacent entries to fill the future gap it will leave.
-    entry.older.newer = entry.newer;
-  }
+LRUList.prototype.get = function(key, done) {
+  done = done || function getDoneNoOp() {};
+  var self = this;
 
-  entry.newer = undefined; // Entry will be newest.
+  this.store.get(key, function storeIODone(err, val) {
+    if (err) { done(err); return; } // I/O failed, maintain current list/map.
 
-  // Move current tail will 2nd-to-tail positiion.
-  entry.older = this.tail;
-  if (this.tail) { this.tail.newer = entry; }
+    var entry = self.keymap[key];
+    if (entry === undefined) { done(null); return; } // Key miss.
+    if (entry === self.tail) { done(null, val); return; } // Key already MRU.
 
-  this.tail = entry;
+    self._updateStructForGet(entry);
+    done(null, val);
+  });
 };
 
 /**
@@ -235,26 +232,67 @@ LRUList.prototype.getMulti = function(keys, done) {
 };
 
 /**
- * Promote the key to the tail (MRU). Read the value from storage.
+ * Apply a get/getMulti operation to the list/map structures.
  *
- * @param {string|array} key
+ * @param {object} entry LRUEntry instance.
+ */
+LRUList.prototype._updateStructForGet = function(entry) {
+  if (entry.newer) { // Key has more-recently-used than it.
+    if (entry === this.head) {
+      this.head = entry.newer; // 2nd-to-head is now head.
+    }
+    // Connect adjacent entries to fill the future gap it will leave.
+    entry.newer.older = entry.older;
+  }
+  if (entry.older) { // Key has less-recently-used than it.
+    // Connect adjacent entries to fill the future gap it will leave.
+    entry.older.newer = entry.newer;
+  }
+
+  entry.newer = undefined; // Entry will be newest.
+
+  // Move current tail will 2nd-to-tail positiion.
+  entry.older = this.tail;
+  if (this.tail) { this.tail.newer = entry; }
+
+  this.tail = entry;
+};
+
+/**
+ * Remove the key from the list and key map. Trigger removal of the value.
+ *
+ * @param {string} key
  * @param {function} done
  *   {object} Error instance or null.
- *   {mixed} Value or undefined.
  */
-LRUList.prototype.get = function(key, done) {
-  done = done || function getDoneNoOp() {};
+LRUList.prototype.remove = function(key, done) {
+  done = done || function removeDoneNoOp() {};
   var self = this;
 
-  this.store.get(key, function storeIODone(err, val) {
+  this.store.del(key, function storeIODone(err) {
     if (err) { done(err); return; } // I/O failed, maintain current list/map.
+    self._updateStructForRemove(key);
+    done(null);
+  });
+};
 
-    var entry = self.keymap[key];
-    if (entry === undefined) { done(null); return; } // Key miss.
-    if (entry === self.tail) { done(null, val); return; } // Key already MRU.
+/**
+ * Remove keys from the list and key map. Trigger removal of the values.
+ *
+ * @param {array} keys
+ * @param {function} done
+ *   {object} Error instance or null.
+ */
+LRUList.prototype.removeMulti = function(keys, done) {
+  done = done || function removeDoneNoOp() {};
+  var self = this;
 
-    self._updateStructForGet(entry);
-    done(null, val);
+  this.store.delMulti(keys, function storeIODone(err) {
+    if (err) { done(err); return; } // I/O failed, maintain current list/map.
+    for (var k = 0; k < keys.length; k++) {
+      self._updateStructForRemove(keys[k]);
+    }
+    done(null);
   });
 };
 
@@ -284,44 +322,6 @@ LRUList.prototype._updateStructForRemove = function(key) {
   }
 
   this.size--;
-};
-
-/**
- * Remove keys from the list and key map. Trigger removal of the values.
- *
- * @param {array} keys
- * @param {function} done
- *   {object} Error instance or null.
- */
-LRUList.prototype.removeMulti = function(keys, done) {
-  done = done || function removeDoneNoOp() {};
-  var self = this;
-
-  this.store.delMulti(keys, function storeIODone(err) {
-    if (err) { done(err); return; } // I/O failed, maintain current list/map.
-    for (var k = 0; k < keys.length; k++) {
-      self._updateStructForRemove(keys[k]);
-    }
-    done(null);
-  });
-};
-
-/**
- * Remove the key from the list and key map. Trigger removal of the value.
- *
- * @param {string} key
- * @param {function} done
- *   {object} Error instance or null.
- */
-LRUList.prototype.remove = function(key, done) {
-  done = done || function removeDoneNoOp() {};
-  var self = this;
-
-  this.store.del(key, function storeIODone(err) {
-    if (err) { done(err); return; } // I/O failed, maintain current list/map.
-    self._updateStructForRemove(key);
-    done(null);
-  });
 };
 
 /**
