a9b87426eeb3993e690cfe36e9d4ffcafd73b801
David Smith <codeactual@gmail.com>
2013-05-30 15:49:39 -0700

    chore(grunt): Migrate to grunt-horde

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,11 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  var mochaShelljsOpt = {stdout: true, stderr: false};
-
-  grunt.initConfig({
-    jshint: {
-      src: {
-        files: {
-          src: ['index.js', 'lib/**/*.js']
-        }
-      },
-      grunt: {
-        files: {
-          src: ['Gruntfile.js']
-        }
-      },
-      tests: {
-        options: {
-          expr: true
-        },
-        files: {
-          src: ['test/lib/**/*.js']
-        }
-      },
-      json: {
-        files: {
-          src: ['*.json']
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'dist/lru-list.js': 'dist/lru-list.js'
-        }
-      }
-    },
-    shell: {
-      options: {
-        failOnError: true
-      },
-      build: {
-        command: 'component install --dev && component build --standalone lruList --name lru-list --out dist --dev'
-      },
-      dist: {
-        command: 'component build --standalone lruList --name lru-list --out dist'
-      },
-      test_lib: {
-        options: mochaShelljsOpt,
-        command: "mocha --colors --reporter spec --recursive test/lib"
-      },
-      dox_lib: {
-        command: 'apidox --input lib/lru-list/index.js --output docs/lru-list.md'
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dox', ['shell:dox_lib']);
-  grunt.registerTask('build', ['default', 'shell:build']);
-  grunt.registerTask('dist', ['default', 'shell:dist', 'uglify:dist', 'dox']);
-  grunt.registerTask('test', ['build', 'shell:test_lib']);
+  require('grunt-horde')
+    .create(grunt)
+    .demand('projName', 'lru-list')
+    .demand('instanceName', 'lruList')
+    .demand('klassName', 'LRUList')
+    .loot('node-component-grunt')
+    .attack();
 };
